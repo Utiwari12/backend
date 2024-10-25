45f87294b92655f5b49c54125feefb0bc5fbc7b2
Utiwari12 <motioniitjeeinstitute@gmail.com>
2024-10-25 10:43:08 +0530

    all setup configured

--- 03-backendPro/src/app.js
+++ 03-backendPro/src/app.js
@@ -19,6 +19,14 @@ app.use(express.static("public"))
 //cookie
 app.use(cookieParser())
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration(middleware)
+app.use("/api/v1/users", userRouter)
+
+//http://localhost:8000/api/v1/users/register
 
 
 export {app}

f9231f22cbf112bcb69fd11d01beebd2fc56e60a
Utiwari12 <motioniitjeeinstitute@gmail.com>
2024-10-26 18:02:51 +0530

    fixed cloudinary bugs

--- 03-backendPro/src/utils/cloudinary.js
+++ 03-backendPro/src/utils/cloudinary.js
@@ -23,7 +23,7 @@ import fs from "fs";
         //console.log("file is uploaded on cloudinary",
             //response.url
         //);
-        fs.unlinkSync(localFilePath)
+        //fs.unlinkSync(localFilePath)
         return response;
        } catch (error) {
         fs.unlinkSync(localFilePath) // remove the 

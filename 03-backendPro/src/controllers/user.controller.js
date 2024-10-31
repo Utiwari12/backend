3de40ae10308b607504924aa8b4414b92d6555d0
Utiwari12 <motioniitjeeinstitute@gmail.com>
2024-10-31 15:35:47 +0530

    Add get watch History

--- 03-backendPro/src/controllers/user.controller.js
+++ 03-backendPro/src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 // access and refesh token generation
 
@@ -501,6 +502,65 @@ const logoutUser = asyncHandler(async(req, res) =>{
     }
   )
 
+  //to add watch history
+
+  const getWatchHistory = asyncHandler(
+    async(req, res) => {
+      const user = await User.aggregate([
+        {
+          $match: {
+            //_id: new mongoose.Types.ObjectId(req.user._id)
+           _id: mongoose.Types.ObjectId(req.user._id)
+          }
+        },
+        {
+          $lookup: {
+            from: "Video", // check here video or videos
+            localField: "watchHistory",
+            foreignField: "_id",
+            as: "watchHistory",
+            pipeline: [
+              {
+                $lookup: {
+                  from: "users",
+                  localField: "owner",
+                  foreignField: "_id",
+                  as: "owner",
+                  pipeline: [
+                    {
+                      $project: {
+                        fullName: 1,
+                        username: 1,
+                        avatar: 1
+                      }
+                    }
+                  ]
+                }
+              },
+              {
+                $addFields: {
+                  owner: {
+                    $first: "$owner"
+                  }
+                }
+              }
+            ]
+          }
+        }
+      ])
+
+      return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          user[0].watchHistory,
+          "Watch History Patched Successfully"
+        )
+      )
+    }
+  )
+
 
 export {
   registerUser,
@@ -512,5 +572,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
-  getUserChannelProfile
+  getUserChannelProfile,
+  getWatchHistory
 }
